93bdfba3a7dcbf6b367952192e17e91f9d22a15a
zhuiyi <awaitm@qq.com>
2018-04-10 06:47:16 +0800

    v1.2.3

--- scrapetest.py
+++ scrapetest.py
@@ -1,8 +1,21 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError, URLError
 from bs4 import BeautifulSoup
 
-html = urlopen("http://pythonscraping.com/pages/page1.html")
-bs0bj = BeautifulSoup(html.read(), "lxml")
-
-print(bs0bj.h1)
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except (HTTPError, URLError) as e:
+        return None
+    try:
+        bs0bj = BeautifulSoup(html.read(),"lxml")
+        title = bs0bj.body.h1
+    except AttributeError as e:
+        return None
+    return title
+title = getTitle("http://pythonscraping.com/pages/page1.html")
+if title == None:
+    print('Title could not be found')
+else:
+    print(title)
 
