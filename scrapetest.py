bbdb5d4f0dffebda8641131ac98c131ecc0ce56c
zhuiyi <awaitm@qq.com>
2018-04-09 21:32:47 +0800

    v1.2.2

--- scrapetest.py
+++ scrapetest.py
@@ -1,6 +1,8 @@
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
 html = urlopen("http://pythonscraping.com/pages/page1.html")
+bs0bj = BeautifulSoup(html.read(), "lxml")
 
-print(html.read())
+print(bs0bj.h1)
 
