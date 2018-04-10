2a080ce5fa5baa8caa87444b37c84b642c31e4c8
zhuiyi <awaitm@qq.com>
2018-04-10 21:11:22 +0800

    v2.4

--- secondary.py
+++ secondary.py
@@ -1,7 +1,10 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import re
 
 html = urlopen("http://www.pythonscraping.com/pages/page3.html")
 bs0bj = BeautifulSoup(html,"lxml")
+images = bs0bj.findAll("img",{"src":re.compile("\.\.\/img\/gifts\/img.*\.jpg")})
 
-print(bs0bj.find("img",{"src":"../img/gifts/img1.jpg"}).parent.previous_sibling.get_text())
+for image in images:
+    print(image["src"])
