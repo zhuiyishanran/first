bcfd8255710b6811300b377962047bc06d211223
zhuiyi <aiscly@gmail.com>
2018-04-12 07:51:17 +0800

    v3.1.1

--- wikipedia.py
+++ wikipedia.py
@@ -1,9 +1,17 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import datetime
+import random
 import re
 
-html = urlopen("http://en.wikipedia.org/wiki/Kevin_Bacon")
-bs0bj = BeautifulSoup(html, "lxml")
-for link in bs0bj.find("div",{"id":"bodyContent"}).findAll("a",href=re.compile("^(/wiki/)((?!:).)*$")):
-    if 'href' in link.attrs:
-        print(link.attrs['href'])
+random.seed(datetime.datetime.now())
+def getLinks(articleUrl):
+    html = urlopen("http://en.wikipedia.org"+articleUrl)
+    bs0bj = BeautifulSoup(html)
+    return bs0bj.find("div",{"id":"bodyContent"}).findAll("a",href=re.compile("^(/wiki/)((?!:).)*$"))
+links = getLinks("/wiki/Kevin_Bacon")
+while len(links)>0:
+    newArticle = links[random.randint(0, len(links)-1)].attrs["href"]
+    print(newArticle)
+    links = getLinks(newArticle)
+    
